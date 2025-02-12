b81c1c126b8ef4d0fbb5199ce9d0de6c27749dde
ianpatricck <ianpatrick499@gmail.com>
2025-02-12 13:51:17 -0300

    feat(DI): inserted boardController to be exported

--- src/di-config.php
+++ src/di-config.php
@@ -4,10 +4,12 @@
 // | Exporta os controladores com DI            |
 // |============================================|
 
+use App\Controllers\BoardController;
 use App\Controllers\UserController;
 
 use function DI\create;
 
 return [
-    'userController' => create(UserController::class)
+    'userController' => create(UserController::class),
+    'boardController' => create(BoardController::class)
 ];

e47262b6edc90de7fc8ad0c9bfcb43365fffcc90
ianpatricck <ianpatrick499@gmail.com>
2025-02-11 21:43:47 -0300

    fix(board): renamed to 'boardRepository'

--- src/Usecases/Board/FindBoardUsecase.php
+++ src/Usecases/Board/FindBoardUsecase.php
@@ -8,11 +8,11 @@ use App\Entities\Board;
 class FindBoardUsecase
 {
     public function __construct(
-        protected BoardRepository $userRepository
+        protected BoardRepository $boardRepository
     ) {}
 
     public function execute(int $id): Board|null
     {
-        return $this->userRepository->findOneById($id);
+        return $this->boardRepository->findOneById($id);
     }
 }

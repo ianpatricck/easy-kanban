5c6e7af02bd23a388ad6a7680a424b42e3a31999
ianpatricck <ianpatrick499@gmail.com>
2025-02-16 11:08:46 -0300

    feat: created DeleteCardUsecase class

---
+++ src/Usecases/Card/DeleteCardUsecase.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace App\Usecases\Card;
+
+use App\Data\Repositories\CardRepository;
+use Exception;
+
+class DeleteCardUsecase
+{
+    public function __construct(
+        private CardRepository $cardRepository
+    ) {}
+
+    public function execute(int $id): void
+    {
+        $card = $this->cardRepository->findOne($id);
+
+        if (!$card) {
+            throw new Exception('The card could not be found', 400);
+        }
+
+        $this->cardRepository->delete($id);
+    }
+}

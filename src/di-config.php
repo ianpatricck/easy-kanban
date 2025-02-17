28a9564d7b6ff417d43f86ee3721e65a91e31785
ianpatricck <ianpatrick499@gmail.com>
2025-02-16 22:22:05 -0300

    feat: returning the CardController with DI

--- src/di-config.php
+++ src/di-config.php
@@ -5,11 +5,13 @@
 // |============================================|
 
 use App\Controllers\BoardController;
+use App\Controllers\CardController;
 use App\Controllers\UserController;
 
 use function DI\create;
 
 return [
     'userController' => create(UserController::class),
-    'boardController' => create(BoardController::class)
+    'boardController' => create(BoardController::class),
+    'cardController' => create(CardController::class),
 ];

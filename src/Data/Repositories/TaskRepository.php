3af35b44d31d407512f70490ff14f706919ee0e4
ianpatricck <ianpatrick499@gmail.com>
2025-02-24 20:48:54 -0300

    feat(TaskRepository): created a task repository class

---
+++ src/Data/Repositories/TaskRepository.php
@@ -0,0 +1,55 @@
+<?php declare(strict_types=1);
+
+namespace App\Data\Repositories;
+
+use App\Data\DAO;
+use App\DTO\CreateTaskDTO;
+use App\Entities\Task;
+
+class TaskRepository
+{
+    public function __construct(
+        private readonly DAO $dao
+    ) {}
+
+    public function findOne(int $id): Task|null
+    {
+        $query = 'SELECT * FROM tasks WHERE id = ?';
+        $task = $this->dao->fetchOne($query, [$id]);
+
+        if ($task) {
+            $taskArray = get_object_vars($task);
+            $taskEntity = new Task(...array_values($taskArray));
+            return $taskEntity;
+        }
+
+        return null;
+    }
+
+    public function findMany(int $limit): array
+    {
+        $query = 'SELECT * FROM tasks LIMIT ?';
+        $tasks = $this->dao->fetchMany($query, [$limit]);
+
+        if (!empty($tasks)) {
+            $taskEntities = [];
+
+            foreach ($tasks as $task) {
+                $taskArray = get_object_vars($task);
+                $taskEntities[] = new Task(...array_values($taskArray));
+            }
+
+            return $taskEntities;
+        }
+
+        return [];
+    }
+
+    public function create(CreateTaskDTO $dto): void
+    {
+        $query = 'INSERT INTO tasks (title, body, hex_bgcolor, owner, attributed_to, card)
+                VALUES (?, ?, ?, ?, ?, ?)';
+
+        $this->dao->execute($query, get_object_vars($dto));
+    }
+}

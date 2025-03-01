d847f43fe5a21636c1667484181c6a6c303e9ac4
ianpatricck <ianpatrick499@gmail.com>
2025-03-01 16:14:48 -0300

    feat(Comment): creted getter methods

--- src/Entities/Comment.php
+++ src/Entities/Comment.php
@@ -12,4 +12,34 @@ class Comment
         private string $created_at,
         private string $updated_at,
     ) {}
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getBody(): string
+    {
+        return $this->body;
+    }
+
+    public function getOwner(): int
+    {
+        return $this->owner;
+    }
+
+    public function getTask(): int
+    {
+        return $this->task;
+    }
+
+    public function getCreatedAt(): string
+    {
+        return $this->created_at;
+    }
+
+    public function getUpdatedAt(): string
+    {
+        return $this->updated_at;
+    }
 }

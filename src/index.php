bd67bbddccd108da98774d2b4cb6ce8c53511a78
ianpatricck <ianpatrick499@gmail.com>
2025-03-08 13:56:14 -0300

    feat: setting up Twig config

--- src/index.php
+++ src/index.php
@@ -6,6 +6,8 @@
 
 use DI\ContainerBuilder;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/utils/isAuthorizedUser.php';
@@ -21,6 +23,12 @@ $app->addErrorMiddleware(true, true, true);
 $appContainerBuilder = new ContainerBuilder();
 $appContainerBuilder->addDefinitions(__DIR__ . '/di-config.php');
 $appContainer = $appContainerBuilder->build();
+
+/*
+ * Configurando o Twig template engine.
+ */
+$twig = Twig::create(__DIR__ . '/', ['cache' => false]);
+$app->add(TwigMiddleware::create($app, $twig));
 
 require __DIR__ . '/api.php';
 

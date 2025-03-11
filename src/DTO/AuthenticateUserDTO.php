313199c21d27f864dd2f7fa789b6d86fe15afdf4
ianpatricck <ianpatrick499@gmail.com>
2025-03-10 21:39:40 -0300

    refactor: added an OpenApi Schema to AuthenticateUserDTO class

--- src/DTO/AuthenticateUserDTO.php
+++ src/DTO/AuthenticateUserDTO.php
@@ -6,10 +6,26 @@
 
 namespace App\DTO;
 
+use OpenApi\Attributes as OA;
+
+#[OA\Schema(
+    title: 'AuthenticateUserDTO',
+    description: 'Authenticate an user'
+)]
 class AuthenticateUserDTO
 {
     public function __construct(
+        #[OA\Property(
+            title: 'email',
+            format: 'email',
+            example: 'john@example.com'
+        )]
         public string $email,
+
+        #[OA\Property(
+            title: 'password',
+            example: 'johnpass2123'
+        )]
         public string $password,
     ) {}
 }

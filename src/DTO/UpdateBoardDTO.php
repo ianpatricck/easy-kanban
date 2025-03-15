bee7b87c0b9686435cb889512a1cc001f2e20a42
ianpatricck <ianpatrick499@gmail.com>
2025-03-15 20:34:24 -0300

    docs: implemented OpenAPI documentation on UpdateBoardDTO

--- src/DTO/UpdateBoardDTO.php
+++ src/DTO/UpdateBoardDTO.php
@@ -1,15 +1,37 @@
 <?php declare(strict_types=1);
 
 // |============================================|
-// | DTO para atualizar um usuário              |
+// | DTO para atualizar um quadro               |
 // |============================================|
 
 namespace App\DTO;
 
+use OpenApi\Attributes as OA;
+
+#[OA\Schema(
+    title: 'UpdateUserDTO',
+    description: 'Update a board'
+)]
 class UpdateBoardDTO
 {
     public function __construct(
+        #[OA\Property(
+            title: 'name',
+            example: 'My updated board'
+        )]
         public string $name,
+
+        #[OA\Property(
+            title: 'owner',
+            example: 1,
+            format: 'int64'
+        )]
+        public int $owner,
+
+        #[OA\Property(
+            title: 'description',
+            example: 'This is a simple updated board'
+        )]
         public ?string $description = null,
     ) {}
 }

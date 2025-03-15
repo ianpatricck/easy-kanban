06cb9c6aa6418132d4dd98e6cd84b4820f88949d
ianpatricck <ianpatrick499@gmail.com>
2025-03-15 10:54:35 -0300

    docs: implemented an OpenApi documentation on CreateBoardDTO

--- src/DTO/CreateBoardDTO.php
+++ src/DTO/CreateBoardDTO.php
@@ -6,12 +6,39 @@
 
 namespace App\DTO;
 
+use OpenApi\Attributes as OA;
+
+#[OA\Schema(
+    title: 'CreateBordDTO',
+    description: 'Create a board'
+)]
 class CreateBoardDTO
 {
     public function __construct(
+        #[OA\Property(
+            title: 'name',
+            example: 'A Simple Board'
+        )]
         public string $name,
+
+        #[OA\Property(
+            title: 'owner',
+            format: 'int64',
+            example: 1
+        )]
         public int $owner,
+
+        #[OA\Property(
+            title: 'description',
+            example: 'This is my first board'
+        )]
         public ?string $description = null,
+
+        #[OA\Property(
+            title: 'active users',
+            format: 'int64',
+            example: 1
+        )]
         public int $active_users = 0
     ) {}
 }

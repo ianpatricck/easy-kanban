d8c3ac5cebd6d346488e6522cbacc9c77e64aa00
ianpatricck <ianpatrick499@gmail.com>
2025-03-16 20:41:24 -0300

    implemented Swagger API documentation at CreateCardDTO

--- src/DTO/CreateCardDTO.php
+++ src/DTO/CreateCardDTO.php
@@ -6,11 +6,32 @@
 
 namespace App\DTO;
 
+use OpenApi\Attributes as OA;
+
+#[OA\Schema(
+    title: 'CreateCardDTO',
+    description: 'Create a board'
+)]
 class CreateCardDTO
 {
     public function __construct(
+        #[OA\Property(
+            title: 'name',
+            example: 'Backlog'
+        )]
         public string $name,
+
+        #[OA\Property(
+            title: 'hex_bgcolor',
+            example: '#e1e1e1'
+        )]
         public string $hex_bgcolor,
+
+        #[OA\Property(
+            title: 'board',
+            format: 'int64',
+            example: 1
+        )]
         public int $board,
     ) {}
 }

edf9c06e0df83a01c6287cbe6460cffd0d4bc247
ianpatricck <ianpatrick499@gmail.com>
2025-03-26 18:41:47 -0300

    Swagger documentation on UpdateTaskDTO class

--- src/DTO/UpdateTaskDTO.php
+++ src/DTO/UpdateTaskDTO.php
@@ -6,12 +6,38 @@
 
 namespace App\DTO;
 
+use OpenApi\Attributes as OA;
+
+#[OA\Schema(
+    title: 'UpdateTaskDTO',
+    description: 'Update a task'
+)]
 class UpdateTaskDTO
 {
     public function __construct(
+        #[OA\Property(
+            title: 'title',
+            example: 'Updated Task'
+        )]
         public string $title,
+
+        #[OA\Property(
+            title: 'body',
+            example: 'New task asked'
+        )]
         public string $body,
+
+        #[OA\Property(
+            title: 'hex_bgcolor',
+            example: '#dddddd'
+        )]
         public string $hex_bgcolor,
+
+        #[OA\Property(
+            title: 'attributed_to',
+            format: 'int64',
+            example: 1,
+        )]
         public int $attributed_to,
     ) {}
 }
